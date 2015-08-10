2fc270a600ec69aca7ab69f68c38eaeeb377687a
DennisLiu <dennis.liu@isturay.com>
2015-08-10 16:40:02 -0700

    finish protocol sample and two node communicate

--- FlowData.py
+++ FlowData.py
@@ -4,8 +4,13 @@ import numpy as np
 from scipy.stats import rv_discrete
 import datetime as dt
 
+# protocol samples
+protocols = ['iec104', 'modbus', 'dnp3', 'opcda', 'profinetio', 'TIAS_TETRA', 's7', 'mms', 'udp', 'ftp', 'tcp']
+protocol_sample = dict()
+for p in protocols:
+    with open('config/'+p+'sample', 'r') as f:
+        protocol_sample[p] = f.read().splitlines()
 
-protocols = ['iec104', 'modbus', 'dnp3', 'opcda', 'profinetio', 'TIAS_TETRA', 's7', 'mms', 'udp', 'icmp', 'ftp', 'tcp']
 
 # network node
 class Node:
@@ -19,44 +24,29 @@ class Node:
         self.proset = proset  # dict {'protocol': weight}
         self.type = type
 
-    def commMsg(self, dest):
+    def commMsg(self, dest, time):
         """
-        communicate with other node and generate flow data
-        :param dest: other node
-        :return: a list of flow data
+        messages between two nodes
+        :param dest: node receiving the message
+        :param time: time generating the message
+        :return: flow data
         """
-        commpro = list(set(self.proset.keys()).intersection(set(dest.proset.keys())))
-        wt = np.array([node1.proset[x] for x in commpro]) + np.array([node2.proset[x] for x in commpro])
+        prols = list(set(self.proset.keys()).intersection(set(dest.proset.keys())))
+        wt = np.array([self.proset[x] for x in prols]) + np.array([dest.proset[x] for x in prols])
         wt /= float(wt.sum())
-        n = np.random.randint(1, 100)  # number of communications to generate
 
-        proid = rv_discrete(values=(range(len(commpro)), wt)).rvs(size=n)
+        protocol = prols[rv_discrete(values=(range(len(prols)), wt)).rvs(size=1)]
         dataflow = []
 
-        for i in range(n):
-            protocol = commpro[proid[i]]
-
-            if protocol == 'iec104':
-                msg = iec104_sample[np.random.randint(len(iec104_sample))]
-
-            if protocol == 'modbus':
-                msg = modbus_sample[np.random.randint(len(modbus_sample))]
-
-            if protocol == 'dnp3':
-                msg = dnp3_sample[np.random.randint(len(dnp3_sample))]
-
-            if protocol == 'opcda':
-                msg = opcda_sample[np.random.randint(len(opcda_sample))]
-
-            if protocol == 'profinetio':
-                msg = profinetio_sample[np.random.randint(len(profinetio_sample))]
-
-            dataflow.append(', '.join([self.ip, dest.ip, msg, dt.datetime.now().strftime('%m/%d/%Y-%H:%M:%S.%f'),
-                                       '1', self.mac, dest.mac]))
-            dataflow.append(', '.join([dest.ip, self.ip, msg, dt.datetime.now().strftime('%m/%d/%Y-%H:%M:%S.%f'),
-                                       '1', dest.mac, self.mac]))
+        msg = protocol_sample[protocol][np.random.randint(len(protocol_sample[protocol]))]
+        dataflow.append(', '.join([self.ip, dest.ip, msg, time, '1', self.mac, dest.mac]))
+        dataflow.append(', '.join([dest.ip, self.ip, msg, time, '1', dest.mac, self.mac]))
 
         return dataflow
+
+# network
+class Network:
+    
 
 
 def ipList(n):
@@ -79,29 +69,13 @@ def macList(n):
     return mac_list
 
 if __name__ == '__main__':
-    # ips = ipList(2)
-    # macs = macList(2)
-    # node1 = Node(ips[0], macs[0], {'tcp': 1., 'iec104': 1., 'modbus': 1.}, 'workStation')
-    # node2 = Node(ips[1], macs[1], {'iec104': 1., 'modbus': 1.}, 'PLC')
-    #
-    # data = node1.commMsg(node2)
-    # for m in data:
-    #     print m
+    ips = ipList(2)
+    macs = macList(2)
+    node1 = Node(ips[0], macs[0], {'tcp': 1., 'iec104': 1., 'modbus': 1.}, 'workStation')
+    node2 = Node(ips[1], macs[1], {'tcp': 1., 'modbus': 1.}, 'PLC')
 
-    # with open('config/profinetiosample', 'w') as f:
-    #     for item in profinetio_sample:
-    #         f.write('%s\n' % item)
+    t = dt.datetime.now().strftime('%m/%d/%Y-%H:%M:%S.%f')
 
-    with open('config/iec104sample', 'r') as f:
-        iec104_sample = f.read().splitlines()
-    with open('config/modbussample', 'r') as f:
-        modbus_sample = f.read().splitlines()
-    with open('config/opcdasample', 'r') as f:
-        opcda_sample = f.read().splitlines()
-    with open('config/profinetiosample') as f:
-        profinetio_sample = f.read().splitlines()
-
-    print iec104_sample
-    print modbus_sample
-    print opcda_sample
-    print profinetio_sample
+    data = node1.commMsg(node2, t)
+    for m in data:
+        print m
